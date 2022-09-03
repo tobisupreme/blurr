8733e5bbfa655d9f44b7380a5b1707d25eda483e
Yemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-09-03 17:07:42 +0100

    init tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import react from "react";
+import Header from "./components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
